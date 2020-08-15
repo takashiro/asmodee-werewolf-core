a27a494914ba7bc1eb4d7c610af3e65caeaa36b5
Kazuichi Takashiro <takashiro@qq.com>
2020-08-15 17:16:56 +0800

    Add interface PlayerProfile

---
+++ src/PlayerProfile.ts
@@ -0,0 +1,8 @@
+import Role from './Role';
+
+interface PlayerProfile {
+	seat: number;
+	roles: Role[];
+}
+
+export default PlayerProfile;
--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { default as Mode } from './Mode';
 export { default as Role } from './Role';
 export { default as Team } from './Team';
 export { default as Teamship } from './Teamship';
+export { default as PlayerProfile } from './PlayerProfile';
