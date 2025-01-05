ae0b60a172a2ce369b62e97ba89be6bd5c078bb8
Kazuichi Takashiro <takashiro@qq.com>
2025-01-05 23:00:06 +0800

    Define abstract class `Skill`

---
+++ src/core/game/Skill.ts
@@ -0,0 +1,51 @@
+import type EventListener from '../driver/EventListener.js';
+
+export abstract class Skill<DriverType, PlayerType> {
+	protected finished = false;
+
+	constructor(
+		protected readonly driver: DriverType,
+		protected readonly owner: PlayerType,
+		protected priority = 0,
+	) {
+	}
+
+	/**
+	 * @returns Skill owner.
+	 */
+	getOwner(): PlayerType {
+		return this.owner;
+	}
+
+	/**
+	 * A smaller number means a higher priority.
+	 * @returns Skill priority.
+	 */
+	getPriority(): number {
+		return this.priority;
+	}
+
+	/**
+	 * Whether the skill can be used.
+	 */
+	abstract isAvailable(): boolean;
+
+	/**
+	 * Whether the skill can be used against selected players.
+	 * @param selected selected players
+	 */
+	abstract isFeasible(selected: PlayerType[]): boolean;
+
+	/**
+	 * Run the skill against selected players.
+	 * @param selected selected players
+	 */
+	abstract execute(selected: PlayerType[]): Promise<void>;
+
+	/**
+	 * @returns A few skill effects triggered on specific events.
+	 */
+	abstract getEffects(): EventListener<number, unknown>[] | undefined;
+}
+
+export default Skill;
